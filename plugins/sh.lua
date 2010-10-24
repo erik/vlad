10b0353f3c5f622e22cf54fd1c4d985850c5f0b2
boredomist <erik.price16@gmail.com>
2010-10-24 18:43:18 -0400

    sh: redirect stderr to stdout, allow multiple lines

--- plugins/sh.lua
+++ plugins/sh.lua
@@ -2,8 +2,11 @@ PluginName = "sh"
 AdminOnly = true
 
 function plugin()
-    local p = io.popen(args_)
+    local p = io.popen(args_ .. " 2>&1")
     result = p:read("*a")
+    
+    result = string.gsub(result, "\n", "  ")
+    result = string.gsub(result, "\r", "  ")
         
     if result == "" then
         result = "No output."
